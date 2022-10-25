62f5f7bf0a26cdafffbb178240807e804136f494
shenmengyang <smy320924@njfu.edu.cn>
2022-10-25 19:24:41 +0800

    test10-24

---
+++ test_10-24/test_10-24/MaxofTen.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a[10] = { 0 };
+//	int i = 0;
+//	int max = a[0];
+//	while (i < 10)
+//	{
+//		scanf("%d", &a[i]);
+//		i++;
+//	}
+//	int j = 0;
+//	while(j<10)
+//	{
+//		if (a[j] > max) {
+//			max = a[j];
+//		}
+//		j++;
+//	}
+//	printf("%d\n", max);
+//	return 0;
+//}
---
+++ test_10-24/test_10-24/total9.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//		int n = 0;
+//		int count = 0;
+//		for (n = 0; n < 101; n++)
+//		{
+//			if (9 == n % 10 || 9 == n / 10)
+//			{
+//				count++;
+//			}
+//		}
+//		printf("\n一共有%d个9\n", count + 1);
+//}
