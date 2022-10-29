ef133c382932bb468c79ba348efc5eb4166cf93f
shenmengyang <smy320924@njfu.edu.cn>
2022-10-29 18:07:07 +0800

    用函数实现对闰年的判断

---
+++ test_10-26/is_leap_year.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//int is_leap_year(int year)
+//{
+//	if (year % 400 == 0)
+//		return 1;
+//	if (year % 4 == 0 && year % 100 != 0)
+//		return 1;
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		if (is_leap_year(n))
+//		{
+//			printf("YES\n");
+//		}
+//		else
+//		{
+//			printf("NO\n");
+//		}
+//	}
+//	
+//	return 0;
+//}
