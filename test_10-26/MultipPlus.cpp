126b65f075c1a184adba91e9c1b052f767b0a266
shenmengyang <smy320924@njfu.edu.cn>
2022-10-29 18:05:17 +0800

    打印乘法表，行数列数自定义

---
+++ test_10-26/MultipPlus.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int k = 0;
+//	int i = 1, j = 1;
+//	scanf("%d", &k);
+//	for (i = 1; i <= k; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ",j,i, (i * j));
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
