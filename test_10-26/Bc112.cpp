23e1c9b9e71d40735557f66f252d7ae254a81f4a
shenmengyang <smy320924@njfu.edu.cn>
2022-10-29 18:04:37 +0800

    nowcoder

---
+++ test_10-26/Bc112.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	long long sum = 0;
+//	scanf("%d", &n);
+//
+//	for (i = 0;i<=n; i++)
+//	{
+//		sum = sum + i;
+//	}
+//	printf("%ld\n", sum);
+//
+//	return 0;
+//}
---
+++ test_10-26/Bc114.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int t = 0,time=0;
+//	scanf("%d", &n);
+//
+//	if (n < 12)
+//	{
+//		time = 2;
+//	}
+//	else
+//	{
+//		t = n / 12;
+//		time = t * 4 + 2;
+//	}
+//	printf("%d\n", time);
+//	return 0;
+//}
