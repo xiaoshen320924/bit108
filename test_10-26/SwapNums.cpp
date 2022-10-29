2494461b760d3b33c043560aee1e7dacf7a234a4
shenmengyang <smy320924@njfu.edu.cn>
2022-10-29 18:06:41 +0800

    用函数实现两数的交换

---
+++ test_10-26/SwapNums.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//void Swap(int* a, int* b)
+//{
+//	int temp = 0;
+//	temp = *b;
+//	*b = *a;
+//	*a = temp;
+//}
+//
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d %d", &a, &b);
+//	Swap(&a,&b);
+//	printf("%d %d", a, b);
+//	return 0;
+//}
