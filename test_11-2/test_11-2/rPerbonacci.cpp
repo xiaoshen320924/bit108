263a2b67a26b6f6caae017644534719f7d360242
shenmengyang <smy320924@njfu.edu.cn>
2022-11-03 17:55:39 +0800

    递归实现Perbonacci

---
+++ test_11-2/test_11-2/rPerbonacci.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+////递归实现斐波那契数
+//int PB(int n)
+//{
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return PB(n - 1) + PB(n - 2);
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		int r = PB(n);
+//		printf("%d\n", r);
+//	}
+//	
+//	return 0;
+//}
