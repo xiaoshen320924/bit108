bc418bdec4f63f2d8987e94bd87acc7c6c50cb93
shenmengyang <smy320924@njfu.edu.cn>
2022-11-03 17:51:28 +0800

    非递归实现阶乘

---
+++ test_11-2/test_11-2/factorialnr.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//int Fac(int n)
+//{
+//	int i = 1, r = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		r = i * r;
+//	}
+//	return r;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int r = Fac(n);
+//	printf("%d\n", r);
+//	return 0;
+//
+//}
