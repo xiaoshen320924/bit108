6b5031f0f50bea8f8ba7a42161f3b441b3dbd78c
shenmengyang <smy320924@njfu.edu.cn>
2022-11-06 22:14:10 +0800

    test_11-5/交换数组元素

---
+++ test_11-5/test_11-5/SwapArray.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//void swap(int* x, int* y)
+//{
+//	int tmp = *x;
+//	*x = *y;
+//	*y = tmp;
+//}
+// 
+// 
+// 
+//int main()
+//{
+//	int a[] = { 1, 3, 5, 7, 9 };
+//	int b[] = { 2, 4, 6, 8, 10 };
+//	int sz = sizeof(a) / sizeof(a[0]);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		swap(&a[i], &b[i]);//交换数组中的元素
+//	}
+// 
+//	printf("交换后数组a的值：");
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("%d ", a[i]);
+//	}
+//	printf("\n");
+//	printf("交换后数组b的值：");
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("%d ", b[i]);
+//	}
+//	return 0;
+//}
