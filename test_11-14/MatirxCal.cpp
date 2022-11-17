83aeda87218f33ac92d4608fea6ce7f13772e327
shenmengyang <smy320924@njfu.edu.cn>
2022-11-17 18:29:17 +0800

    test_11-14

---
+++ test_11-14/MatirxCal.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+////æÿ’Ûº∆À„
+//int count(int str[100][100], int n, int m)
+//{
+//	int i = 0;
+//	int j = 0;
+//	int count = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			if (str[i][j] > 0)
+//			{
+//				count += str[i][j];
+//			}
+//		}
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int arr[100][100] = { 0 };
+//	int n = 0,m =0;
+//	scanf("%d %d", &n, &m);
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	int num = count(arr, n, m);
+//	printf("%d\n", num);
+//	return 0;
+//}
