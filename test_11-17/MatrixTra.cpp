a4b595cb5861cab87550c0f7e2176deecbdbd2f7
shenmengyang <smy320924@njfu.edu.cn>
2022-11-19 20:11:02 +0800

    矩阵转置

---
+++ test_11-17/MatrixTra.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+////¾ØÕó×ªÖÃ
+//int main()
+//{
+//	int n = 0, m = 0;
+//	int arr[2][3] = { 0 };
+//	scanf("%d %d", &n, &m);
+//	int i = 0, j = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	for (j = 0; j < m; j++) {
+//		for (i = 0; i < n; i++) {
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
