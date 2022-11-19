9a5c283bbe039c7834d150ed8d0a48dd77950a5e
shenmengyang <smy320924@njfu.edu.cn>
2022-11-19 20:18:27 +0800

    进制转换

---
+++ test_11-17/BC111.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+////六进制转化
+//
+//void transform(int n)
+//{
+//	if (n / 6 != 0)
+//	{
+//		transform(n / 6);
+//		printf("%d", n % 6);
+//	}
+//	else
+//	{
+//		printf("%d", n % 6);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	transform(n);
+//	return 0;
+//}
